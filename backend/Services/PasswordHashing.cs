e00c6cdfea06aa70097a3496b79171599185a67e
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-02-23 15:37:01 +0200

    Password Hashing addition

---
+++ backend/Services/PasswordHashing.cs
@@ -0,0 +1,16 @@
+using BCrypt.Net;
+
+namespace Services;
+
+public class PasswordHashingService
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
