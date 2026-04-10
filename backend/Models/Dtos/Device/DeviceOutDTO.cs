347630bf7d6c738e6d2be8f0953fa3b755f40ea5
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-04-10 18:32:10 +0300

    DeviceOut DTO Update

--- backend/Models/Dtos/Device/DeviceOutDTO.cs
+++ backend/Models/Dtos/Device/DeviceOutDTO.cs
@@ -1,21 +1,47 @@
+using System.Text.Json.Serialization;
 using Models;
 
 namespace Dtos;
 
 public sealed class DeviceOutDTO
 {
+    [JsonPropertyName("id")]
     public string Id { get; init; } = default!;
+
+    [JsonPropertyName("uuid")]
+    public Guid Uuid { get; init; }
+
+    [JsonPropertyName("deviceId")]
+    public string DeviceId { get; init; } = default!;
+
+    [JsonPropertyName("name")]
     public string? Name { get; init; }
+
+    [JsonPropertyName("location")]
     public string? Location { get; init; }
-    public DateTime Registered_at { get; init; }
-    public Guid User_Id { get; init; }
+
+    [JsonPropertyName("description")]
+    public string? Description { get; init; }
+
+    [JsonPropertyName("registeredAt")]
+    public DateTime RegisteredAt { get; init; }
+
+    [JsonPropertyName("lastSeenAt")]
+    public DateTime? LastSeenAt { get; init; }
+
+    [JsonPropertyName("userId")]
+    public Guid UserId { get; init; }
 
     public static DeviceOutDTO FromEntity(Device d) => new()
     {
-        Id = d.Id.ToString(),
+        Id = d.DeviceId,
+        Uuid = d.Id,
+        DeviceId = d.DeviceId,
         Name = d.Name,
         Location = d.Location,
-        Registered_at = d.RegisteredAt,
-        User_Id = d.UserId
+        Description = d.Description,
+        RegisteredAt = d.RegisteredAt,
+        LastSeenAt = d.LastSeenAt,
+        UserId = d.UserId
     };
 }
