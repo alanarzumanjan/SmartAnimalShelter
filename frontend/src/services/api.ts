2218e5b3ba0d924ca46a95f5d09645e41950e018
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-04-13 23:22:40 +0300

    Add authorization token interceptor to API requests

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -8,4 +8,13 @@ const api = axios.create({
   },
 });
 
+// Attach Authorization token to every request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
