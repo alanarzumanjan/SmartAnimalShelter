d7fe2b425be1f4f31169976e1e8367feef416bbc
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-04-13 23:20:30 +0300

    Add CatalogController for managing species, genders, and statuses

---
+++ backend/Controllers/Pets/CatalogController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Data;
+
+namespace Controllers;
+
+[ApiController]
+[Route("catalog")]
+public class CatalogController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public CatalogController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet("species")]
+    public async Task<IActionResult> GetSpecies()
+    {
+        var species = await _db.Species
+            .Include(s => s.Breeds)
+            .OrderBy(s => s.Name)
+            .ToListAsync();
+
+        return Ok(species);
+    }
+
+    [HttpGet("genders")]
+    public async Task<IActionResult> GetGenders()
+    {
+        var genders = await _db.Genders
+            .OrderBy(g => g.Id)
+            .ToListAsync();
+
+        return Ok(genders);
+    }
+
+    [HttpGet("statuses")]
+    public async Task<IActionResult> GetStatuses()
+    {
+        var statuses = await _db.AdoptionStatuses
+            .OrderBy(s => s.Id)
+            .ToListAsync();
+
+        return Ok(statuses);
+    }
+}
