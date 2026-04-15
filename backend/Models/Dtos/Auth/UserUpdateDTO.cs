7963bc29d0f858b6fbbfd025c72c05679ae1c5eb
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-04-15 22:05:44 +0300

    Add address property to UserUpdateDto for user information updates

--- backend/Models/Dtos/Auth/UserUpdateDTO.cs
+++ backend/Models/Dtos/Auth/UserUpdateDTO.cs
@@ -5,5 +5,6 @@ public class UserUpdateDto
     public string? name { get; set; }
     public string? email { get; set; }
     public string? phone { get; set; }
+    public string? address { get; set; }
     public string? role { get; set; }
 }
