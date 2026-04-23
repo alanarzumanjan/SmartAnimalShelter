0bf4dcf707f2fe9458cfbcd2ba676736c979f9ec
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-04-24 02:07:56 +0300

    Refactor Docker CI workflow and backend run script for clarity and consistency in messaging

--- backend/run.sh
+++ backend/run.sh
@@ -6,13 +6,13 @@ echo "🔍 Port check $PORT..."
 PID=$(sudo lsof -t -i:$PORT 2>/dev/null)
 
 if [ -n "$PID" ]; then
-    echo "⚠️ Port is occupied by process $PID. Killing..."
+    echo "Port is occupied by process $PID. Killing..."
     sudo kill -9 $PID 2>/dev/null
     sleep 0.5
-    echo "✅ Port is now free."
+    echo "Port is now free."
 else
-    echo "✅ Port is free."
+    echo "Port is free."
 fi
 
-echo "🚀 Run..."
+echo "Run..."
 dotnet run
