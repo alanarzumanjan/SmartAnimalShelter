eafaa577f0d1a280238607a6c8cafca72a28bc60
Alan Arzumanjan <alanarzumanjan@gmail.com>
2026-04-27 00:47:44 +0300

    Remove ShelterOwnerId column from Shelters table in migration

---
+++ backend/Migrations/20260426202218_RemoveShelterOwnerId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartShelter.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveShelterOwnerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShelterOwnerId",
+                table: "Shelters");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ShelterOwnerId",
+                table: "Shelters",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
